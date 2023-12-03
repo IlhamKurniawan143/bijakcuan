a5f7936c0b7a388c00a92a8311c027af396804cb
IlhamKurniawan143 <ilhamkur143@gmail.com>
2023-12-03 18:20:13 +0700

    update halaman promo dan artikel

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { Helmet } from "react-helmet"
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
 import Beranda from "./pages/Beranda"
+import Profil from "./pages/profile/Profil"
 import Masuk from "./pages/Masuk"
 import Daftar from "./pages/Daftar"
 import Program from "./pages/Program"
 import Checkout from "./pages/Checkout"
-import PembayaranSukses from "./pages/PembayaranSukses"
-import PembayaranGagal from "./pages/PembayaranGagal"
 import Event from "./pages/Event"
 import DetailEvent from "./pages/DetailEvent"
 import Promo from "./pages/Promo"
@@ -17,32 +16,132 @@ import Bantuan from "./pages/Bantuan"
 import Kontak from "./pages/Kontak"
 import Artikel from "./pages/Artikel"
 import DetailArtikel from "./pages/DetailArtikel"
+import PembayaranSukses from "./pages/PembayaranSukses"
+import Kursus from "./pages/Kursus"
+
+const MainLayout = ({ children }) => (
+	<>
+		<Navbar />
+		{children}
+		<Footer />
+	</>
+)
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Helmet>
-				<title>Bijakcuan.</title>
+				<title>Bijakcuan | Pelatihan Keuangan untuk Sukses Finansial</title>
 			</Helmet>
-			<Navbar />
 			<Routes>
-				<Route path="/" element={<Beranda />} />
+				<Route
+					path="/"
+					element={
+						<MainLayout>
+							<Beranda />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/program"
+					element={
+						<MainLayout>
+							<Program />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/event"
+					element={
+						<MainLayout>
+							<Event />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/checkout"
+					element={
+						<MainLayout>
+							<Checkout />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/event/:id"
+					element={
+						<MainLayout>
+							<DetailEvent />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/promo"
+					element={
+						<MainLayout>
+							<Promo />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/artikel"
+					element={
+						<MainLayout>
+							<Artikel />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/detail/:id"
+					element={
+						<MainLayout>
+							<DetailArtikel />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="*"
+					element={
+						<MainLayout>
+							<NotFound />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/bantuan"
+					element={
+						<MainLayout>
+							<Bantuan />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/kontak"
+					element={
+						<MainLayout>
+							<Kontak />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/profil"
+					element={
+						<MainLayout>
+							<Profil />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/kursus"
+					element={
+						<MainLayout>
+							<Kursus />
+						</MainLayout>
+					}
+				/>
+				<Route path="/checkout/sukses" element={<PembayaranSukses />} />
 				<Route path="/masuk" element={<Masuk />} />
 				<Route path="/daftar" element={<Daftar />} />
-				<Route path="/program" element={<Program />} />
-				<Route path="/event" element={<Event />} />
-				<Route path="/checkout" element={<Checkout />} />
-				<Route path="/pembayaransukses" element={<PembayaranSukses />} />
-				<Route path="/pembayarangagal" element={<PembayaranGagal />} />
-				<Route path="/event/detail" element={<DetailEvent />} />
-				<Route path="/promo" element={<Promo />} />
-				<Route path="/artikel" element={<Artikel />} />
-				<Route path="/detail/:id" element={<DetailArtikel />} />
-				<Route path="*" element={<NotFound />} />
-				<Route path="/bantuan" element={<Bantuan />} />
-				<Route path="/kontak" element={<Kontak />} />
 			</Routes>
-			<Footer />
 		</BrowserRouter>
 	)
 }
--- src/pages/PembayaranSukses.jsx
+++ src/pages/PembayaranSukses.jsx
@@ -1,47 +1,28 @@
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons"
+import Helmet from "react-helmet"
 
 export default function PembayaranSukses() {
-  return (
-	<main id="pembayaran">
-	<div className="container align-items-center d-flex justify-content-center">
-		<div className="container-lg text-center" >
-			<br />
-			<br />
-			<FontAwesomeIcon 
-				icon={faCircleCheck} 
-				className="mb-4"
-				style={{
-					color: "Green",
-					borderRadius: "2rem",
-<<<<<<< HEAD
-					height: "150px",
-					width: "150px",
-=======
-					height: "100px",
-					width: "100px",
->>>>>>> 0ec10432cf1c945300f4eebce8601e8e79c44aa5
-				}}></FontAwesomeIcon>
-				
-			<h1 className="mb-4">Pembayaran Sukses</h1>
-			<p className="mb-5 w-75 mx-auto">
-<<<<<<< HEAD
-				Silahkan mempelajari materi kelas yang telah <br />kami design dengan baik
-=======
-				Silahkan mempelajari materi kelas yang telah <br/>kami design dengan baik
->>>>>>> 0ec10432cf1c945300f4eebce8601e8e79c44aa5
-				untuk mencapai goals Anda
-			</p>
-			<Link href={"/profil"} className="btn btn-primary">
-				Mulai Belajar
-			</Link>
-			<br />
-			<br />
-        	<br />
-            <br />
-		</div>
-	</div>
-</main>
-  )
+	return (
+		<main id="Sukses">
+			<Helmet>
+				<title>Pembayaran Sukses | Bijakcuan.</title>
+			</Helmet>
+			<div className="container align-items-center d-flex justify-content-center">
+				<div className="container-lg text-center">
+					<img
+						className="img-fluid"
+						src="/assets/pembayaran-sukses/sukses.png"
+					/>
+					<h1 className="mb-4">Pembayaran Sukses</h1>
+					<p className="mb-5 w-75 mx-auto">
+						Silahkan mempelajari materi kelas yang telah kami design dengan baik
+						untuk mencapai goals
+					</p>
+					<Link href={"/beranda"} className="btn btn-primary">
+						Mulai Belajar
+					</Link>
+				</div>
+			</div>
+		</main>
+	)
 }
